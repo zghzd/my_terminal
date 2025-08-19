e9ca5ff0817d1fecf2b00463f3883664e1a40ccd
z z <zfg_0362@outlook.com>
2025-08-19 18:54:55 +0800

    修改“compare”功能的实现方法

--- file_compare.h
+++ file_compare.h
@@ -16,61 +16,31 @@ private:
         std::cin >> in_file_name;
         return file_read(in_file_name);
     }
-    pair<int, pair<int, int>> findLongestCommonSubstring(const string& s1, const string& s2) {
-        int m = s1.size(), n = s2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        int max_len = 0, end1 = 0, end2 = 0;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                    if (dp[i][j] > max_len) {
-                        max_len = dp[i][j];
-                        end1 = i;
-                        end2 = j;
-                    }
-                }
-            }
-        }
-        return { max_len, {end1 - max_len, end2 - max_len} };
-    }
 public:
     static void file_compare_stringtext(bool is_debug) {
-        my_compare text_0, text_1, findLongestCommonSubstring_;
+        my_compare text_0, text_1;
         string text_a = text_0.in_file_data_stringtext(is_debug);
         string text_b = text_1.in_file_data_stringtext(is_debug);
-        auto splitToLines = [](const string& input) {
-            vector<string> lines;
-            istringstream iss(input);
-            string line;
-            while (getline(iss, line)) {
-                lines.push_back(line);
-            }
-            return lines;
-            };
-        vector<string> lines_a = splitToLines(text_a);
-        vector<string> lines_b = splitToLines(text_b);
-        size_t max_lines = max(lines_a.size(), lines_b.size());
-        cout << "base=first file\n";
-        for (size_t i = 0; i < max_lines; ++i) {
-            string s1 = (i < lines_a.size()) ? lines_a[i] : "";
-            string s2 = (i < lines_b.size()) ? lines_b[i] : "";
-            if (i < lines_a.size() && i < lines_b.size()) {
-                auto result = findLongestCommonSubstring_.findLongestCommonSubstring(s1, s2);
-                int len = result.first;
-                auto pos = result.second;
-                int start1 = pos.first;
-                int start2 = pos.second;
-                string removed = s1.substr(0, start1);
-                string added = s2.substr(start2 + len);
-                cout << "Line " << i + 1 << ":\n-=" << removed << "\n+=" << added << endl;
-            }
-            else if (i < lines_a.size()) {
-                cout << "Line " << i + 1 << " (only in base):\n-=" << s1 << "\n+=" << endl;
+        std::replace(text_b.begin(), text_b.end(), '\r', '\n');
+        std::vector<std::string>text_compare_b;
+        std::vector<std::string>text_compare_a;
+        while (std::getline(std::cin, text_a)) {
+            text_compare_a.push_back(text_a);
+        }
+        while (std::getline(std::cin, text_b)) {
+            text_compare_b.push_back(text_b);
+        }
+        for (size_t i = 0; i < text_compare_b.size(); ++i) {
+            if (text_a.find(text_compare_b[i]) != string::npos) {
+                auto new_end = std::remove(text_compare_a.begin(), text_compare_a.end(), text_compare_b[i]);
+                text_compare_a.erase(new_end, text_compare_a.end());
             }
             else {
-                cout << "Line " << i + 1 << " (only in other):\n-=\n+=" << s2 << endl;
+                std::cout << "+=" << text_compare_b[i] << std::endl;
             }
+        }
+        for (size_t i = 0; i < text_compare_a.size(); ++i) {
+            std::cout << "-=" << text_compare_a[i] << std::endl;
         }
     }
 };
