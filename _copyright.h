7560aa7696a0b58f5429bff584731da38cee2f54
z z <zfg_0362@outlook.com>
2025-09-06 23:00:01 +0800

    程序内版权和版本信息

--- _copyright.h
+++ _copyright.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include <iostream>
 void _copyright() {
+	const std::string _version = "0.2.1+20250906.all";
 	const std::string _copyright = "翊 的控制台\nCopyright (c) 2025 zghzd\nLicensed under the Apache License, Version 2.0\n";
+	std::cerr << _version << std::endl;
 	std::cerr << _copyright << std::endl;
 }
